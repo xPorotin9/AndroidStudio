6508f2c09cbd047a276e21e52fd306a879242c72
Porotin <jmachacav@ulasalle.edu.pe>
2024-09-17 21:10:01 -0500

    Update

---
+++ Int-Imagenes/app/src/main/java/com/example/spotifai/ImageAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.spotifai
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+
+class ImageAdapter(private val context: Context, private val images: Array<Int>) : BaseAdapter() {
+
+    override fun getCount(): Int = images.size
+
+    override fun getItem(position: Int): Any = images[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val imageView: ImageView = convertView?.findViewById(R.id.imageView) ?: run {
+            val inflater = LayoutInflater.from(context)
+            val view = inflater.inflate(R.layout.grid_item_image, parent, false)
+            view.findViewById<ImageView>(R.id.imageView)
+        }
+        imageView.setImageResource(images[position])
+        return imageView
+    }
+}
