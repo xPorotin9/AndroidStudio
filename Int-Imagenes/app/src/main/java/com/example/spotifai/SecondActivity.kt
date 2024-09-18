e047369863f37507a8f285fc1f41f583d78d64ab
Porotin <jmachacav@ulasalle.edu.pe>
2024-09-17 21:17:44 -0500

    Update

--- Int-Imagenes/app/src/main/java/com/example/spotifai/SecondActivity.kt
+++ Int-Imagenes/app/src/main/java/com/example/spotifai/SecondActivity.kt
@@ -3,7 +3,6 @@ package com.example.spotifai
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
