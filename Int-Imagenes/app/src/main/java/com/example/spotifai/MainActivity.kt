2aef494a2982713a684cc08439034b49cb496b04
Porotin <jmachacav@ulasalle.edu.pe>
2024-09-23 02:07:34 -0500

    Actualización de MainActivity para implementar fragmentos - Se reemplaza la lógica de GridView por el uso de ImageGridFragment para mejorar la modularidad. - Se elimina la navegación a SecondActivity, gestionando la visualización de imágenes directamente en el fragmento. - Se añade manejo de savedInstanceState para preservar el estado de la interfaz al rotar el dispositivo.

--- Int-Imagenes/app/src/main/java/com/example/spotifai/MainActivity.kt
+++ Int-Imagenes/app/src/main/java/com/example/spotifai/MainActivity.kt
@@ -1,32 +1,23 @@
 // Descripción: Interfaz con un Spinner para seleccionar imágenes y un botón para navegar a otra actividad.
 // Autor: José C. Machaca
-// Fecha creación: 15-08
-// Fecha última modificación: 15-09
+// Fecha creación: 21-08
+// Fecha última modificación: 22-09
 
 package com.example.spotifai
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private val images = arrayOf(R.drawable.dogi, R.drawable.sublimeow, R.drawable.spinlog, R.drawable.bruh, R.drawable.calc, R.drawable.claz, R.drawable.door, R.drawable.fondo, R.drawable.plop)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gridView = findViewById<GridView>(R.id.gridViewImages)
-        val adapter = ImageAdapter(this, images)
-        gridView.adapter = adapter
-
-        gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("image_id", images[position])
-            startActivity(intent)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, ImageGridFragment())
+                .commit()
         }
     }
 }
